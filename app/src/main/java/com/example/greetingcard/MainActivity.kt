0a7a1109639ceae03003e856a51869bde654ccd6
Rhiles <125476748+Rhiles@users.noreply.github.com>
2025-04-23 15:18:00 -0500

    Assn 6 Commit

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.greetingcard
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -12,22 +16,56 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val permission = "com.example.greetingcard.MSE412"
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private var permissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            permissionGranted = isGranted
+            if (!isGranted) {
+                Toast.makeText(this, "Permission is required to continue", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Check permission at runtime
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionLauncher.launch(permission) // <-- added
+        } else {
+            permissionGranted = true // <-- added
+        }
+
         setContent {
             GreetingCardTheme {
                 MainScreen(
                     onExplicitClick = {
-                        val intent = Intent(this, SecondActivity::class.java)
-                        startActivity(intent)
-                    },
+                        if (permissionGranted) {
+                            val intent = Intent(this, SecondActivity::class.java)
+                            startActivity(intent)
+                        }
+                        else {
+                            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        },
                     onImplicitClick = {
-                        val intent = Intent("com.example.OPEN_SECOND_ACTIVITY")
-                        startActivity(intent)
+                        if (permissionGranted) {
+                            val intent = Intent("com.example.OPEN_SECOND_ACTIVITY")
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     },
                     onViewImageClick = {
                         val intent = Intent(this, ImageActivity::class.java)
